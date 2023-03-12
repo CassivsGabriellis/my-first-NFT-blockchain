edb38a54efecb8e202ec6bbcbea904c9c4638f76
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-03-12 18:22:40 -0300

    Refactored contract code: it has now a price (10 wei minimum)

--- contracts/MyFirstNFT.sol
+++ contracts/MyFirstNFT.sol
@@ -12,17 +12,19 @@ contract MyFirstNFT is ERC721, Ownable {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
 
-    constructor() public ERC721("MyFirstNFT", "NFT") {}
+    constructor() public ERC721("MyFirstNFT", "MFNFT") {}
 
-    function mintNFT(
-        address recipient,
-        string memory tokenURI
-    ) public onlyOwner returns (uint256) {
+    function mintToken(
+        address to,
+        string memory uri
+    ) public payable virtual returns (uint256) {
+        require(msg.value >= 10, "Not enough ETH sent; check price!");
+
         _tokenIds.increment();
-
         uint256 newItemId = _tokenIds.current();
-        _mint(recipient, newItemId);
-        _setTokenURI(newItemId, tokenURI);
+
+        _mint(to, newItemId);
+        _setTokenURI(newItemId, uri);
 
         return newItemId;
     }
