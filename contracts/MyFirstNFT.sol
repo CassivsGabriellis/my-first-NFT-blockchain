4639ff901af0f637109fbcb33a9f2700c44eb8e9
Cassivs Gabriellis <cassiogabrielcontato@gmail.com>
2023-03-12 16:46:28 -0300

    added a comment in the contract body

--- contracts/MyFirstNFT.sol
+++ contracts/MyFirstNFT.sol
@@ -2,6 +2,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.7.0;
 
+// Importing the OpenZepellin library models
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
